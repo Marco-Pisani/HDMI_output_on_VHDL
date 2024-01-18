9991735a8a56c3bdb0a0978f713f57651c9de61c
Marco-Pisani <61452584+Marco-Pisani@users.noreply.github.com>
2024-01-18 14:18:59 +0100

    Update and rename cpu_test.vhd to toppest_lvl.vhd

--- toppest_lvl.vhd
+++ toppest_lvl.vhd
@@ -1,35 +1,9 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/01/2023 12:18:58 PM
--- Design Name: 
--- Module Name: cpu_test - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity cpu_test is
     Port (
