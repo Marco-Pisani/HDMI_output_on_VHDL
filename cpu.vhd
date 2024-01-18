bd5db56fb25d2229798acb059e99e93d23f3d278
Marco-Pisani <61452584+Marco-Pisani@users.noreply.github.com>
2024-01-18 14:20:54 +0100

    Update cpu.vhd

--- cpu.vhd
+++ cpu.vhd
@@ -2,16 +2,9 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+--simulates a cpu that writes to a memory
 entity cpu is
     Port (
         data: out std_logic_vector(23 downto 0);
